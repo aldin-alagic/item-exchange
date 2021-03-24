619152b5999a2cb2d355e84926bd0dbe82afca9a
aldin-alagic <aldin@dysordian.com>
2021-03-24 22:27:06 +0100

    Add Index and Create action methods to ItemController

--- ItemExchange/ItemExchange/Controllers/ItemController.cs
+++ ItemExchange/ItemExchange/Controllers/ItemController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ItemExchange.Data;
+using ItemExchange.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,32 @@ namespace ItemExchange.Controllers
 {
     public class ItemController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ItemController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
+        {
+            IEnumerable<Item> items = _db.Items;
+            return View(items);
+        }
+
+        public IActionResult Create()
         {
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Item item)
+        {
+            _db.Items.Add(item);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
