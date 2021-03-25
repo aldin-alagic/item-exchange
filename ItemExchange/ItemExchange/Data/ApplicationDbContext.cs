6874e34b0fd409ff03e269e337dd8820b3112e2f
aldin-alagic <aldin@dysordian.com>
2021-03-26 00:17:16 +0100

    Add ExpenseType DB set to Application DB context

--- ItemExchange/ItemExchange/Data/ApplicationDbContext.cs
+++ ItemExchange/ItemExchange/Data/ApplicationDbContext.cs
@@ -16,6 +16,6 @@ namespace ItemExchange.Data
 
         public DbSet<Item> Items { get; set; }
         public DbSet<Expense> Expenses { get; set; }
-
+        public DbSet<ExpenseType> ExpenseTypes { get; set; }
     }
 }
