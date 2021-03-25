68e6fc630e018e4830b98856d2ed0de78b4b3ba6
aldin-alagic <aldin@dysordian.com>
2021-03-26 00:22:52 +0100

    Create an ExpenseType controller with Index, Create, Update and Delete action methods

---
+++ ItemExchange/ItemExchange/Controllers/ExpenseTypeController.cs
@@ -0,0 +1,77 @@
+﻿using ItemExchange.Data;
+using ItemExchange.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItemExchange.Controllers
+{
+    public class ExpenseTypeController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseTypeController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<ExpenseType> expenseTypes = _db.ExpenseTypes;
+            return View(expenseTypes);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(ExpenseType expenseType)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ExpenseTypes.Add(expenseType);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(expenseType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int? id)
+        {
+            var expensType = _db.ExpenseTypes.Find(id);
+            if (expensType is null) return NotFound();
+            _db.ExpenseTypes.Remove(expensType);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Update(int? id)
+        {
+            if (id is null || id == 0) return NotFound();
+            var expensType = _db.ExpenseTypes.Find(id);
+            if (expensType is null) return NotFound();
+           
+            return View(expensType);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(ExpenseType expenseType)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ExpenseTypes.Update(expenseType);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(expenseType);
+        }
+    }
+}
