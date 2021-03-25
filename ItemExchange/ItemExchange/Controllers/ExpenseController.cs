51a8f0d66349832acd1ac6722313b2e7c6b55897
aldin-alagic <aldin@dysordian.com>
2021-03-25 01:42:49 +0100

    Create Expense controller with Index and Create action methods

---
+++ ItemExchange/ItemExchange/Controllers/ExpenseController.cs
@@ -0,0 +1,44 @@
+﻿using ItemExchange.Data;
+using ItemExchange.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItemExchange.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpenseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expense> expenses = _db.Expenses;
+            return View(expenses);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense expense)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Expenses.Add(expense);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(expense);
+        }
+    }
+}
