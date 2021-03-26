df0baebfa1c51590f7bd5418fb9f955b220ae204
aldin-alagic <aldin@dysordian.com>
2021-03-26 02:42:18 +0100

    Add and run migrations for ExpenseType foreign key in Expense Model

--- ItemExchange/ItemExchange/Migrations/20210325233343_expenseTypeToExpenseModel.Designer.cs
+++ ItemExchange/ItemExchange/Migrations/20210325233343_expenseTypeToExpenseModel.Designer.cs
@@ -3,14 +3,16 @@ using ItemExchange.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ItemExchange.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210325233343_expenseTypeToExpenseModel")]
+    partial class expenseTypeToExpenseModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ItemExchange/ItemExchange/Migrations/20210325233343_expenseTypeToExpenseModel.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ItemExchange.Migrations
+{
+    public partial class expenseTypeToExpenseModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Expenses",
+                newName: "Name");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TypeId",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Expenses_TypeId",
+                table: "Expenses",
+                column: "TypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_ExpenseTypes_TypeId",
+                table: "Expenses",
+                column: "TypeId",
+                principalTable: "ExpenseTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_ExpenseTypes_TypeId",
+                table: "Expenses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Expenses_TypeId",
+                table: "Expenses");
+
+            migrationBuilder.DropColumn(
+                name: "TypeId",
+                table: "Expenses");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Expenses",
+                newName: "Type");
+        }
+    }
+}
