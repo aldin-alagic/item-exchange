2364d9d52595cce387238662af041293d20d2767
aldin-alagic <aldin@dysordian.com>
2021-03-26 02:42:39 +0100

    Create an Expense view model

---
+++ ItemExchange/ItemExchange/ViewModels/ExpenseViewModel.cs
@@ -0,0 +1,15 @@
+﻿using ItemExchange.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ItemExchange.ViewModels
+{
+    public class ExpenseViewModel
+    {
+        public Expense Expense { get; set; }
+        public IEnumerable<SelectListItem> ExpenseTypes { get; set; }
+    }
+}
